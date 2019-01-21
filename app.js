c99d0e8636f20650e51eb23965182af1f4cedea9
Piotr Jaczewski <pp.jaczewski@gmail.com>
2019-01-21 21:10:24 +0100

    praca nad api ciąg dalszy

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ req.open('GET', 'http://www.apilayer.net/api/live?access_key=ba1feed0be86f73c95f
 req.onreadystatechange = function (aEvt) {
 if (req.readyState == 4) {
     if(req.status == 200)
-    console.log(req.response);
+    makeData(req.response);
+    // console.log(req.response);
     
     else
     console.log("Błąd podczas ładowania danych\n");
@@ -11,3 +12,30 @@ if (req.readyState == 4) {
 };
 req.send(null);
 
+
+// TEST =================================================================================
+            // Tabela
+            var makeData = function(item) {
+    
+                var data = JSON.parse(item);
+                var currencies = data.quotes;
+                // var currencies = data['quotes'];
+                // var output = `<table class="mainTable"><thead><tr><th>Currency</th><th>Value</th></tr></thead>`; 
+
+
+                Object.keys(currencies).forEach(function(key) {
+                    console.log(key, currencies[key]);
+
+                    // for(var i = 0; i < currencies.length; i++) {
+                    //     output += "<tr><td>" + key[i] + "</td>"; 
+                    //     output += '<td>' + currencies[key] + '</td></tr>'; 
+                    // }
+
+                    // output += "</table>"; 
+
+                    // document.getElementsByClassName('mainTable').innerHTML = output;
+                });
+              
+              }
+              
+// TEST =================================================================================
