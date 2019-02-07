6db03513d71f034d27e6b811873af7c696c26b28
Piotr Jaczewski <pp.jaczewski@gmail.com>
2019-02-07 19:30:11 +0100

    przeniesienie danych do select

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ req.open('GET', 'http://www.apilayer.net/api/live?access_key=ba1feed0be86f73c95f
 req.onreadystatechange = function (aEvt) {
 if (req.readyState == 4) {
     if(req.status == 200)
-    makeData(req.response);
     // console.log(req.response);
+    //  var test = JSON.parse(req.response);
+     makeData(req.response);
+
     
     else
     console.log("Błąd podczas ładowania danych\n");
@@ -12,31 +14,61 @@ if (req.readyState == 4) {
 };
 req.send(null);
 
+// ================================================================================================ TEST 1
 
-// TEST =================================================================================
+// function makeData(data) {
+//     var test = JSON.parse(data);
+//     var currencies = test.quotes;
+//     console.log(currencies);
+
+//     var output = `<table class="tabelkaWalut"><thead><tr><th>Waluta</th><th>Konwersja</th></tr></thead>`; 
+
+//     Object.keys(currencies).forEach(function(key) {
+
+//         output += "<tr><td>" + key + "</td>"; 
+//         output += '<td>' + currencies[key] + '</td></tr>'; 
+    
+//     });
+
+//     output += "</table>"; 
+//     document.querySelector('.content').innerHTML = output;
+    
+// }
+    
+// ================================================================================================ TEST 2
+
 function makeData(data) {
     var test = JSON.parse(data);
     var currencies = test.quotes;
-    console.log(currencies);
+    // console.log(currencies);
 
-    // Object.keys(currencies).forEach(function(key) {
 
-    //     console.log(key, currencies[key]);
 
-    // });
-
-    var output = `<table class="tabelkaWalut"><thead><tr><th>Waluta</th><th>Konwersja</th></tr></thead>`; 
+    var output = `<select class="selectCurrency" id="test">`; 
 
     Object.keys(currencies).forEach(function(key) {
+        var keyItem = String(key);
+        var targetCurrency = keyItem.replace(/USD/g,"");
 
-        output += "<tr><td>" + key + "</td>"; 
-        output += '<td>' + currencies[key] + '</td></tr>'; 
+        output += "<option data-value=" + currencies[key] +">" + targetCurrency + "</td>"; 
     
     });
 
-    output += "</table>"; 
+    output += "</select>"; 
     document.querySelector('.content').innerHTML = output;
     
 }
-              
-// TEST =================================================================================
+
+function getSelectedValue() {
+    var selectedValue = document.getElementById("test").dataset.value;
+    console.log(selectedValue);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    console.log("DOM został wczytany");
+    console.log("Tutaj dopiero wyłapujemy elementy");
+    
+    var currSelect = document.getElementById("test");
+    currSelect.addEventListener("change",getSelectedValue);
+});
